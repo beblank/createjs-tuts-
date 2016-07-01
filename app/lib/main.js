1582ee2a86f268da5f39fa1af81d45ab32254f76
beblank <kulik2@ymail.com>
2016-07-01 18:18:32 +0800

    add ticker

--- app/lib/main.js
+++ app/lib/main.js
@@ -2,23 +2,33 @@ var utils = require('./utils'),
 	domReady = utils.domReady;
 
 var c = createjs;
+var stage = new c.Stage('main');
 
 console.log('Game Started: EaselJS version: ' + c.EaselJS.version);
 
 domReady(function(){
-	var stage = new c.Stage('main');
+	var group = new c.Container();
+	group.x = 200;
+	group.y = 175;
+	stage.addChild(group);
 
 	var circle = new c.Shape();
 	circle.graphics.beginFill('green').drawCircle(0,0,50);
 	circle.x = 100;
 	circle.y = 100;
-	stage.addChild(circle);
+	group.addChild(circle);
 
 	var square = new c.Shape();
 	square.graphics.beginFill('green').drawRect(0,0,50,50);
 	square.x = 200;
 	square.y = 100;
-	stage.addChild(square);
+	group.addChild(square);
 
-	stage.update();
+	c.Ticker.timingMode = c.Ticker.RAF;
+	c.Ticker.setFPS(60);
+	c.Ticker.addEventListener('tick', onUpdate);
 });
+
+function onUpdate(){
+	stage.update();
+}
