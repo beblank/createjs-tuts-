4035914eaec99e6584182201a3580a9cc1c7df48
beblank <kulik2@ymail.com>
2016-07-04 10:40:26 +0800

    chapter 2-2 complete

--- app/lib/main.js
+++ app/lib/main.js
@@ -10,6 +10,8 @@ domReady(function(){
 	var group = new c.Container();
 	group.x = 200;
 	group.y = 175;
+	group.regX = 100;
+	group.regY = 100;
 	stage.addChild(group);
 
 	group.on('tick', onGroupUpdate);
@@ -21,10 +23,22 @@ domReady(function(){
 	group.addChild(circle);
 
 	var square = new c.Shape();
-	square.graphics.beginFill('green').drawRect(0,0,50,50);
+	square.graphics.beginFill('green')
+			.drawRect(0,0,50,50)
+			.endFill()
+			.beginStroke("rgba(0,0,0,1)")
+			.moveTo(5,75)
+			.bezierCurveTo(45,90,75,75,-25,-25)
+			.endStroke();
 	square.x = 200;
 	square.y = 100;
 	group.addChild(square);
+
+	var introText = new c.Text('Welcome to the game', '20px Arial', 'ff7700');
+	introText.x = 200;
+	introText.y = 300;
+	introText.lineWidth = 100;
+	stage.addChild(introText);
 
 	c.Ticker.timingMode = c.Ticker.RAF;
 	c.Ticker.setFPS(60);
